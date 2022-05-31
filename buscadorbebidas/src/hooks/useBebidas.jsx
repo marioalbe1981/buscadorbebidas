2f682924da2c32d0e6cf112cd12e50ce46cc9097
marioalbe1981 <73264376+marioalbe1981@users.noreply.github.com>
2022-05-31 14:42:56 -0300

    Add files via upload

---
+++ buscadorbebidas/src/hooks/useBebidas.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import BebidasContext from '../context/BebidasProvider'
+
+const useBebidas = () => {
+  return (
+    useContext(BebidasContext)
+  )
+}
+
+export default useBebidas
